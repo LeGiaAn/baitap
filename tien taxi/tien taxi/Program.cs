c5725ca8d382fdd9b853b5e18a3a6803f09e2754
An <andoycle@gmail.com>
2021-12-04 11:15:26 +0700

    baitap

---
+++ tien taxi/tien taxi/Program.cs
@@ -0,0 +1,26 @@
+﻿namespace Statement
+{
+    class Program
+    {
+        static void Main(string[] args) {
+            Console.WriteLine("nhap day so muon xem so chan toi da la 10 so: ");
+            int[] a = new int[10];
+            for (int i = 0; i < 10; i++)
+            {
+                a[i] = int.Parse(Console.ReadLine());
+            }
+            for (int i = 0;i < 10;i++)
+            {
+                if ( a[i] % 2 == 0)
+                {
+                    Console.WriteLine( + a[i]);
+                }
+            }
+               
+            
+        }
+    }
+}
+
+
+
